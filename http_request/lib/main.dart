c20b029b024738f24847f31ba1fa15213a7f4d45
Aisya <aisyanwarputri@gmail.com>
2021-06-15 00:24:25 +0700

    push hari ini

--- http_request/lib/main.dart
+++ http_request/lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,17 +31,12 @@ class _HomePageState extends State<HomePage> {
         title: Text("News App", style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
       ),
-
-      //Now let's call the APi services with futurebuilder wiget
       body: FutureBuilder(
         future: client.getArticle(),
         builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
-          //let's check if we got a response or not
           if (snapshot.hasData) {
-            //Now let's make a list of articles
             List<Article> articles = snapshot.data;
             return ListView.builder(
-              //Now let's create our custom List tile
               itemCount: articles.length,
               itemBuilder: (context, index) =>
                   customListTile(articles[index], context),
--- http_request/lib/services/api_service.dart
+++ http_request/lib/services/api_service.dart
@@ -3,33 +3,33 @@ import 'dart:convert';
 import 'package:http_request/model/article_model.dart';
 import 'package:http/http.dart';
 
-//Now let's make the HTTP request services
-// this class will alows us to make a simple get http request
-// from the API and get the Articles and then return a list of Articles
+//Sekarang mari kita buat layanan permintaan HTTP
+// kelas ini akan memungkinkan kita untuk membuat permintaan get http sederhana
+// dari API dan dapatkan Artikel lalu kembalikan daftar Artikel
 
 class ApiService {
-  //let's add an Endpoint URL, you can check the website documentation
-  // and learn about the different Endpoint
-  //for this example I'm going to use a single endpoint
+// mari tambahkan URL Endpoint, Anda dapat memeriksa dokumentasi situs web
+  // dan pelajari tentang Titik Akhir yang berbeda
+  //untuk contoh ini saya akan menggunakan satu titik akhir
 
-  //NOTE: make sure to use your OWN apikey, you can make a free acount and
-  // choose a developer option it's FREE
+  //CATATAN: pastikan untuk menggunakan apikey SENDIRI, Anda dapat membuat akun gratis dan
+  // pilih opsi pengembang, GRATIS
   final endPointUrl =
       "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={ADD YOUR API KEY HERE}";
 
-  //Now let's create the http request function
-  // but first let's import the http package
+//Sekarang mari kita buat fungsi permintaan http
+  // tapi pertama-tama mari kita impor paket http
 
   Future<List<Article>> getArticle() async {
     Response res = await get(endPointUrl);
 
-    //first of all let's check that we got a 200 statu code: this mean that the request was a succes
+//pertama-tama mari kita periksa apakah kita mendapat kode status 200: ini berarti permintaannya berhasil
     if (res.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(res.body);
 
       List<dynamic> body = json['articles'];
 
-      //this line will allow us to get the different articles from the json file and putting them into a list
+//baris ini akan memungkinkan kita untuk mendapatkan artikel yang berbeda dari file json dan memasukkannya ke dalam daftar
       List<Article> articles =
           body.map((dynamic item) => Article.fromJson(item)).toList();
 
